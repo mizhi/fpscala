41921958e2abf0e577a92d2b83b5bb559e931595
Mitchell Peabody <mizhi@cognius.com>
2015-11-24 00:59:12 -0500

    Chapter 3 exercises

---
+++ src/com/mizhi/fpscala/Chapter3.sc
@@ -0,0 +1,71 @@
+import com.mizhi.fpscala._
+
+// Exercise 3.2, p35
+val l = List("foo", "bar", "baz")
+List.tail(List.tail(l))
+
+// Exercise 3.3, p36
+List.setHead(l, "laff")
+
+// Exercise 3.4, p36
+List.drop(l, 1)
+
+// Exercise 3.5, p36
+val l2 = List(1,2,3,4,5,6,7,8)
+List.dropWhile[Int](l2, x => x < 5)
+List.dropWhile2[Int](l2, x => x < 5)
+List.dropWhile3(l2)(x => x < 5)
+
+// Exercise 3.6, p36
+List.init(l2)
+
+// Exercise 3.8, p40
+List.foldRight(List(1,2,3), Nil:List[Int])(Cons(_, _))
+List.foldRight(List(1,2,3), Nil:List[Int])((a, b) => Cons(a, b))
+
+// Exercise 3.9, p40
+List.foldRight(List(1,2,3,4,5), 0)((a, b) => 1 + b)
+
+// Exercise 3.11, p40
+List.foldLeft(List(1,2,3), 0)(_ + _) // sum
+List.foldLeft(List(1,2,3,4), 1)(_ * _) // product
+List.foldLeft(List(1,2,3), 0)((a, b) => a + 1) // length
+
+// Exercise 3.12, p41
+List.reverse(List(1,2,3))
+
+// Exercise 3.13, p41
+List.foldRight2(List(1,2,3), Nil:List[Int])(Cons(_, _)) // Should be the identity
+
+// Exercise 3.14, p41
+List.append(List(1,2,3), List(4))
+
+// Exercise 3.15, p41
+List.concatAll(List(
+  List(1,2,3),
+  List(4,5,6),
+  List(7,8,9)
+))
+
+// Exercise 3.16, p42
+List.foldRight2(List(1,2,3), Nil:List[Int])((a, b) => Cons(a + 1, b))
+
+// Exercise 3.17, p42
+List.foldRight2(List(1.0,2.0,3.0), Nil:List[String])((a, b) => Cons(a.toString, b))
+// Exercise 3.18, p42
+List.map(List(1.0, 2.0, 3.0))(n => n.toString)
+// Exercise 3.19, p42
+List.filter(List(1,2,3,4,5))(_ < 4)
+
+// Exercise 3.20, p42
+List.flatMap(List(1,2,3))(i => List(i,i))
+
+// Exercise 3.21, p43
+List.filter2(List(1,2,3,4,5))(_ < 4)
+
+// Exercise 3.22, p43
+List.addLists(List(1,2,3), List(9,8,7))
+
+// Exercise 3.23, p43
+List.zipWith(List(1,2,3), List("2", "3", "4"))((a,b) => a + b.toInt)
+
---
+++ src/com/mizhi/fpscala/Chapter3.scala
@@ -0,0 +1,136 @@
+package com.mizhi.fpscala
+
+import scala.annotation.tailrec
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] = {
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+  }
+
+  // Exercise 3.4, p35
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_, tail) => tail
+    }
+  }
+
+  // Exercise 3.3, p36
+  def setHead[A](l: List[A], h: A): List[A] = {
+    Cons(h, tail(l))
+  }
+
+  // Exercise 3.4, p36
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0) l
+    else drop(tail(l), n - 1)
+  }
+
+  // Exercise 3.5, p36
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => if (f(h)) dropWhile(tail(l), f) else l
+    }
+  }
+
+  def dropWhile2[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(h, t) if f(h) => dropWhile2(t, f)
+      case _ => l
+    }
+  }
+
+  def dropWhile3[A](l: List[A])(f: A => Boolean): List[A] = {
+    l match {
+      case Cons(h, t) if f(h) => dropWhile3(t)(f)
+      case _ => l
+    }
+  }
+
+  // Exercise 3.6. p37
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
+  }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  // Exercise 3.10, p40
+  @tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+  }
+
+  // Exercise 3.12, p41
+  def reverse[A](as: List[A]) = foldLeft(as, Nil:List[A])((a, b) => Cons(b, a))
+
+  // Exercise 3.13, p41
+  def foldRight2[A,B](as: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(reverse(as), z)((b: B, a: A) => f(a, b))
+  }
+
+  // Exercise 3.14, p41
+  def append[A](as: List[A], bs: List[A]): List[A] = {
+    foldRight2(as, bs)(Cons(_, _))
+  }
+
+  // Exercise 3.15, p41
+  def concatAll[A](als: List[List[A]]): List[A] = {
+    foldLeft(als, Nil:List[A])(append(_, _))
+  }
+
+  // Exercise 3.18, p42
+  def map[A,B](as: List[A])(f: A => B): List[B] = {
+    foldRight2(as, Nil:List[B])((a, b) => Cons(f(a), b))
+  }
+
+  // Exercise 3.19, p42
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    foldRight2(as, Nil:List[A])((a, b) => if (f(a)) Cons(a, b) else b)
+  }
+
+  // Exercise 3.20, p42
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = {
+    foldRight2(as, Nil:List[B])((a, b) => append(f(a), b))
+  }
+
+  // Exercise 3.21, p43
+  def filter2[A](as: List[A])(f: A => Boolean): List[A] = {
+    flatMap(as)((a) => if (f(a)) List(a) else Nil)
+  }
+
+  // Exercise 3.22, p43
+  def addLists(as: List[Int], bs:List[Int]): List[Int] = {
+    (as, bs) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, addLists(xs, ys))
+    }
+  }
+
+  // Exercise 3.23, p43
+  def zipWith[A,B,C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = {
+    (as, bs) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+    }
+  }
+}
